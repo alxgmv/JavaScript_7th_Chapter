64dbbcf91b5cdc594aa342904fd79dcfadaf591f
alxgmv <alxgmv@gmail.com>
2020-01-15 15:14:32 +0100

    update_3

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function Transport(vehicleType, fuelType, power) {
   }
 Transport.prototype.fullName = function () {
     return 'type: ' + this.vehicleType + ' | '
-    + 'fuel :' + this.fuelType + ' | '
-    + 'power :' + this.power + ' Hp';
+    + 'fuel: ' + this.fuelType + ' | '
+    + 'power: ' + this.power + ' Hp';
 }
 
 function Plane(vehicleType, fuelType, power, cabinWidth, wingLength) {
@@ -20,19 +20,27 @@ Plane.prototype.wingSpan = function() {
   let wn =  this.wingLenth*2 + this.cabinWidth
   return wn;
 }
-// let x = (Plane.prototype.wingSpan();
 Plane.prototype.localTitle = function() {
   return 'type: ' + this.vehicleType + ' |'
-  + 'fuel :' + this.fuelType + ' |'
-  + 'power :' + this.power + ' Hp' + ' |'
-  + 'wingspan :' + (this.wingLenth*2 + this.cabinWidth) + ' m';
+  + 'fuel: ' + this.fuelType + ' |'
+  + 'power: ' + this.power + ' Hp' + ' |'
+  + 'wingspan: ' + (this.wingLenth*2 + this.cabinWidth) + ' m';
 }
 
 function Car(vehicleType, fuelType, power, cabinType) {
-  Transport.apply(this,arguments);
+  Transport.apply(this, arguments);
   console.log(arguments);
   this.cabinType = cabinType;
 }
+
+function Bike(vehicleType, travel, frameSize) {
+  Transport.call(this, vehicleType);
+  console.log(arguments);
+  this.travel = travel;
+  this.frameSize = frameSize;
+}
+
+
 let ICE = new Transport ('train', 'electricity', 2400);
 console.log(ICE.fullName());
 
@@ -43,3 +51,10 @@ console.log(golfstreamF500.localTitle());
 
 let dodgeRam = new Car('car', 'diesel', 245, 'pickup');
 dodgeRam.fuelType = 'gas';
+
+let specializedEnduro = new Bike('bike', 160, 56);
+specializedEnduro.fullName = function () {
+    return 'type: ' + this.vehicleType + ' | '
+    + 'travel: ' + this.travel + ' mm | '
+    + 'frameSize: ' + this.frameSize + ' cm';
+}
